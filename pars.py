2534bc5cb624307a509c6c5b90bcfaf5ba945088
Nastya <ten_anastasia@mail.ru>
2017-06-30 09:45:51 +0300

    !

--- pars.py
+++ pars.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 html = urlopen('http://realty.dmir.ru/').read()
 soup = BeautifulSoup(html, 'html.parser')
-print(soup)
+my_file = open('str.json', 'w')
+my_file.write(soup)
+my_file.close()
