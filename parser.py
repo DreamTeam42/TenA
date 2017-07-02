13548e8f387540b9ce75a1c0e3bad7e13222817a
Nastya <ten_anastasia@mail.ru>
2017-07-02 20:44:45 +0300

    final version

--- parser.py
+++ parser.py
@@ -55,33 +55,56 @@ def pars(url):
     price = soup.find('span', class_='tag').text.strip().strip()
     list['Цена'] = price
 
-    #meter_price = soup.find('li', class_='meterprice').b.text.strip()
-    #list['Цена за метр'] = meter_price
+    try:
+        meter_price = soup.find('li', class_='meterprice').b.text.strip()
+        list['Цена за метр'] = meter_price
+    except AttributeError:
+        list['Цена за метр'] = "не указана"
 
     count_of_rooms = (soup.find('li', class_='rooms').b.text)
     list['Количество комнат']=count_of_rooms
     all_square = (soup.find('li', class_='square').b.text)
     list['Общая площадь'] = all_square
-    metro = ' '.join([i.strip() for i in soup.find('li', class_='metro').text.split()])
-    if (len(metro)>0):
+    try:
+        metro = ' '.join([i.strip() for i in soup.find('li', class_='metro').text.split()])
         list['Метро'] = metro
-    else:
-        list['Метро'] = "нет"
+    except AttributeError: list['Метро'] = "нет"
+
     floor = ' '.join([i.strip() for i in soup.find('li', class_='floor').text.split()])
     list['Этаж'] = floor
     description = soup.find('div', class_='mb20 objectDesc').text
     list['Описание'] = description
+
     # контактная информация
-    posted = soup.find('a', class_='cap').text.strip()
-    list['Разместил'] = posted
-    # компания
-    if(posted == 'менеджер компании'):
-        corp = soup.find_all('dd')[3].text
-        list['Компания'] = corp
-    date = soup.find_all('dd')[4].text
-    list['Дата размещения'] = date
+    try:
+        posted = soup.find('a', class_='cap').text.strip()
+        list['Разместил'] = posted
+        # компания
+        if(posted == 'менеджер компании'):
+            corp = soup.find_all('dd')[3].text
+            list['Компания'] = corp
+    except AttributeError: list['Разместил'] = "нет"
+
+    try:
+        date = soup.find_all('dd')[4].text
+        list['Дата размещения'] = date
+    except IndexError: list['Дата размещения'] = soup.find_all('dd')[2].text
     phone = soup.find('div', class_='phone').text
     list['Телефон'] = phone
+
+    # сбор информация по карте
+    map_url = soup.find('figure', class_='cardmap').a.attrs['href'][27:].split('|')
+    map_info = {}
+    for item in map_url:
+        ident, value = item.split('=')
+        map_info[ident] = value
+    # информация по карте
+    print(map_info)
+    image_urls = []
+    for image in soup.find_all('input', class_="_imageViewerMini"):
+        image_urls.append(image.attrs['data-full'])
+    list['Адрес'] = image_urls
+
     return list
 
 def main():
@@ -95,16 +118,20 @@ def main():
 
 
 if __name__ == '__main__':
-    main_list = []
+    page = requests.get(start_url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    ad_count = int(soup.find('table', class_='sort-resultlist').tr.th.b.text)
+    main_list = [0]*3
     print('Начал')
 
     list_url = get_list()
-    #for index in list_url:
-    url = list_url[0]
-    #print(url)
-    main_list = pars(url)
+    for index in range(3):
+        url = list_url[index]
+        main_list[index] = pars(url)
 
     print('Закончил')
 
     with open('data.json', 'w', encoding='cp1251') as file:
             json.dump(main_list, file, indent = 2, ensure_ascii = False)
+
+    print('Данные загружены в data.json')
